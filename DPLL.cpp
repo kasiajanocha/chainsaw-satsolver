d98924e4c7680335a1152f5a0ad82adf37220958
phantom10111 <phantom10111@gmail.com>
2016-03-14 16:36:14 +0100

    Add empty separators

--- DPLL.cpp
+++ DPLL.cpp
@@ -23,13 +23,18 @@ bool solve_sat(std::size_t numvar, const std::vector<std::vector<int>> &clauses,
 {
 	if(numvar == assignments.size())
 		return is_model(clauses, assignments);
+
 	assignments.emplace_back(1);
+
 	if(solve_sat(numvar, clauses, assignments))
 		return true;
+
 	assignments.pop_back();
 	assignments.emplace_back(-1);
+
 	if(solve_sat(numvar, clauses, assignments))
 		return true;
+
 	assignments.pop_back();
 	return false;
 }
@@ -37,14 +42,18 @@ bool solve_sat(std::size_t numvar, const std::vector<std::vector<int>> &clauses,
 void solve(std::size_t numvar, const std::vector<std::vector<int>> &clauses)
 {
 	std::vector<int> assignments;
+
 	if(solve_sat(numvar, clauses, assignments))
 	{
 		std::printf("s SATISFIABLE\n");
+
 		for(std::size_t i = 0; i < numvar; ++i)
 		{
 			std::printf("v ");
+
 			if(std::signbit(assignments[i]))
 				std::printf("-");
+
 			std::printf("%zu\n", i + 1);
 		}
 	}
@@ -62,36 +71,46 @@ int main()
 	{
 		if(std::fgets(line, sizeof(line), stdin) == nullptr)
 			break;
+
 		if(line[0] != 'p')
 			continue;
+
 		if(std::sscanf(line, "p %s%zu%zu", format, &numvar, &numclause) != 3)
 		{
 			std::printf("Bad input line: %s\n", line);
 			return -1;
 		}
+
 		if(std::strcmp(format, "cnf") != 0)
 		{
 			std::printf("Bad format: %s\n", format);
 			return -1;
 		}
+
 		for(std::size_t i = 0; i < numclause; ++i)
 		{
 			std::vector<int> clause;
+
 			while(true)
 			{
 				int var;
+
 				if(std::scanf("%d", &var) != 1)
 				{
 					std::printf("Bad input\n");
 					return -1;
 				}
+
 				if(var == 0)
 					break;
+
 				clause.emplace_back(var);
 			}
+
 			clauses.emplace_back(std::move(clause));
 		}
 	}
+
 	solve(numvar, clauses);
 	return 0;
 }
